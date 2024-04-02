7dfebafeb95cfa4646797018e055dc2b65a81233
atifkhan9013590 <158748667+atifkhan9013590@users.noreply.github.com>
2024-04-02 04:12:23 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,1 @@
-module.exports = {
-  myEmail: "almarjan366@gmail.com",
-  myPassweord: "rzll zpcw vtkj fosi",
-  secretKey: "your_actual_secret_key",
-  adminSecretKey: "your-secret-key-here",
-  stripeSecretKey:
-    "sk_test_51NIyGYAAqbnw4ie1MO9RzLt9FbJ52j9f4MVKao4AVoRA3rOHZxwENA5adCsbkPyZdwxbN4cg03a8swd8V8DjdHhi00qrERzSjs",
-};
+
